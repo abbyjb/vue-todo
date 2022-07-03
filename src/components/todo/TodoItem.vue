5169e0a450c4e04ca2b7d59f27a236b95198366b
Abby Bashore <>
2022-07-02 20:42:44 -0700

    Complete a TodoItem
    * convert TodoItems to be a checkbox with a label
    * strikethrough text when a TodoItem is marked complete

--- src/components/todo/TodoItem.vue
+++ src/components/todo/TodoItem.vue
@@ -1,12 +1,38 @@
 <template>
-  <li>{{ description }}</li>
+  <li>
+    <input
+      @change="toggleCheckbox"
+      :checked="checked"
+      type="checkbox"
+      id="checkbox"
+    />
+    <label for="checkbox">
+      {{ description }}
+    </label>
+  </li>
 </template>
+
+<style scoped>
+input[type="checkbox"]:checked ~ label {
+  text-decoration: line-through;
+}
+</style>
 
 <script>
 export default {
   name: "TodoItem",
+  data() {
+    return {
+      checked: false,
+    };
+  },
   props: {
     description: String,
+  },
+  methods: {
+    toggleCheckbox(event) {
+      this.checked = event.target.checked;
+    },
   },
 };
 </script>
