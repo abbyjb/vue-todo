202a9e483586eb487d812771b4136c8880b832e7
Abby Bashore <>
2022-08-04 19:35:00 -0700

    install tailwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./styles/tailwind.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
