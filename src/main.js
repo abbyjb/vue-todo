1ea31b975bc738619d2e10508779ab577fd20782
abbyjb <22553870+abbyjb@users.noreply.github.com>
2025-11-17 19:42:32 -0800

    add edit and delete icons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faTrashCan, faPenToSquare);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
